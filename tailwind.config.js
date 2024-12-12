712be8bbdaa49de4d107627a8662dbef42b46087
Neoriat <Neoriat@duck.com>
2024-12-12 15:50:33 +0330

    -Fixed the colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}" , "./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'fav-gray': '#181a1b',
+      },
+    },
   },
   plugins: [],
 }
